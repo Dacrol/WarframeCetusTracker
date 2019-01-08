fb79842d9f033b55d62680a1ad25d7d42cf72eae
Jonas Ahrens <jonas.ahrens@gmail.com>
2019-01-08 11:32:54 +0100

    Add live timer interval

--- src/App.js
+++ src/App.js
@@ -7,13 +7,23 @@ class App extends Component {
   constructor(props) {
     super(props)
     extendObservable(this, {
-      cycleStats: {}
+      cycleStats: {},
+      timeLeft: 0
     })
+    this.timeInterval = null
     this.updateCetusCycle()
   }
 
   render() {
-    return this.cycleStats && <div>{JSON.stringify(this.cycleStats)}</div>
+    return (
+      this.cycleStats && (
+        <div>
+          {JSON.stringify(this.cycleStats)}
+          <p>Currently it is: {this.cycleStats.isDay ? 'day' : 'night'}</p>
+          <p>Time left: {msToTime(this.timeLeft)}</p>
+        </div>
+      )
+    )
   }
 
   updateCetusCycle() {
@@ -22,8 +32,26 @@ class App extends Component {
       .then(data => {
         this.cycleStats = data
         console.log(this.cycleStats)
+        if (this.timeInterval) clearInterval(this.timeInterval)
+        this.timeInterval = setInterval(() => {
+          this.updateTimeLeft()
+        }, 1000)
       })
   }
+
+  updateTimeLeft() {
+    this.timeLeft = new Date(this.cycleStats.expiry).getTime() - Date.now()
+  }
+}
+
+function msToTime(s) {
+  let ms = s % 1000
+  s = (s - ms) / 1000
+  let secs = s % 60
+  s = (s - secs) / 60
+  let mins = s % 60
+  let hrs = (s - mins) / 60
+  return hrs + ':' + mins + ':' + secs
 }
 
 export default observer(App)
