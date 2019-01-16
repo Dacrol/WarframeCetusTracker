241b55f630bba3a580d50e1aa6f33f1a5ad0011d
Jonas Ahrens <jonas.ahrens@gmail.com>
2019-01-16 14:37:35 +0100

    Updates

---
+++ precache-manifest.2ddfa8efa8ed5bf10bca33a934bfebe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36b5ae611fcb766d9c87",
+    "url": "/static/js/main.36b5ae61.chunk.js"
+  },
+  {
+    "revision": "a7e9bee15a945818cb99",
+    "url": "/static/js/1.a7e9bee1.chunk.js"
+  },
+  {
+    "revision": "36b5ae611fcb766d9c87",
+    "url": "/static/css/main.44d51cde.chunk.css"
+  },
+  {
+    "revision": "52b8b97e906ea949c778813039caacb2",
+    "url": "/index.html"
+  }
+];
