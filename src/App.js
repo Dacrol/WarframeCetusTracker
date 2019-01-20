7212e56173761d0e77179db56cbc1670080646f3
Jonas Ahrens <jonas.ahrens@gmail.com>
2019-01-20 22:21:13 +0100

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { extendObservable } from 'mobx'
 import { observer } from 'mobx-react'
